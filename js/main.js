61b5c32eb8105b8c0ee5863328c4f841298caa8b
Lorbac <baccini.lorenzo@gmail.com>
2021-07-22 17:13:23 +0200

    Terminata parte html/css + inizio js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var btnCalcolaPrezzo = document.getElementById("nome");
+
+btnCalcolaPrezzo.addEventListener("click",
+    function() {
+        var nome = document.getElementById("nome").value;
+        var km = document.getElementById("km").value;
+        var fascia_eta = document.getElementById("fascia_eta").value;
+    }
+)
